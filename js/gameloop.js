15126f36b936b5f892f6082dd959f49ed9a43fcc
Manan <manan.bharara@gmail.com>
2013-05-23 23:01:27 +0530

    Multiple js files to avoid the chaos

---
+++ js/gameloop.js
@@ -0,0 +1,25 @@
+var player = new Player();
+
+player.setPosition(~~((width - player.width) / 2), ~~((height - player.height) / 2));
+player.jump();
+
+document.onkeydown = function (e) {
+  if (e.which == '37')
+    player.moveLeft();
+  if (e.which == '39')
+    player.moveRight();
+  if (e.which == '40')
+    player.stoop();
+};
+
+
+var GameLoop = function () {
+  clear();
+  moveCircles(5);
+  drawCircles();
+  if (player.isJumping) player.checkJump();
+  if (player.isFalling) player.checkFall();
+  player.draw();
+  gLoop = setTimeout(GameLoop, 1000 / 50);
+};
+GameLoop();
--- js/player.js
+++ js/player.js
@@ -1,50 +1,4 @@
-var width = 320, height = 500, gLoop;
-var c = document.getElementById('c');
-ctx = c.getContext('2d');
-
-c.width = width;
-c.height = height;
-
-var clear = function () {
-  ctx.fillStyle = '#d0e7f9';
-  ctx.beginPath();
-  ctx.rect(0, 0, width, height);
-  ctx.closePath();
-  ctx.fill();
-
-};
-
-var howManyCircles = 10, circles = [];
-
-for (var i = 0; i < howManyCircles; ++i) {
-  circles.push([Math.random() * width, Math.random() * height, Math.random() * 100, Math.random() / 2]);
-}
-
-var drawCircles = function () {
-  for (var i = 0; i < howManyCircles; i++) {
-    ctx.fillStyle = 'rgba(255, 255, 255, ' + circles[i][3] + ')';
-    ctx.beginPath();
-    ctx.arc(circles[i][0], circles[i][1], circles[i][2], 0, Math.PI * 2, true);
-    ctx.closePath();
-    ctx.fill();
-  }
-};
-
-var moveCircles = function (deltaY) {
-  for (var i = 0; i < howManyCircles; i++) {
-    if (circles[i][1] - circles[i][2] > height) {
-      circles[i][0] = Math.random() * width;
-      circles[i][2] = Math.random() * 100;
-      circles[i][1] = 0 - circles[i][2];
-      circles[i][3] = Math.random() / 2;
-    } else {
-      circles[i][1] += deltaY;
-    }
-  }
-};
-
-
-var player = new (function () {
+var Player = function () {
   var thisPlayer = this;
 
   thisPlayer.image = new Image();
@@ -138,27 +92,5 @@ var player = new (function () {
     thisPlayer.setPosition(thisPlayer.X, height - thisPlayer.height);
   };
 
-})();
-
-player.setPosition(~~((width - player.width) / 2), ~~((height - player.height) / 2));
-player.jump();
-
-document.onkeydown = function (e) {
-  if (e.which == '37')
-    player.moveLeft();
-  if (e.which == '39')
-    player.moveRight();
-  if (e.which == '40')
-    player.stoop();
 };
 
-var GameLoop = function () {
-  clear();
-  moveCircles(5);
-  drawCircles();
-  if (player.isJumping) player.checkJump();
-  if (player.isFalling) player.checkFall();
-  player.draw();
-  gLoop = setTimeout(GameLoop, 1000 / 50);
-};
-GameLoop();

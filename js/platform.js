14e1374452194aae2fe2a9ce272200b94822c130
Manan <manan.bharara@gmail.com>
2013-05-25 13:50:53 +0530

    Adding draw method to platform.js

--- js/platform.js
+++ js/platform.js
@@ -1,4 +1,4 @@
-var platform = function (x, y, type) {
+var Platform = function (x, y, type) {
   var thisPlatform = this;
 
   thisPlatform.firstColor = '#FF8C00';
@@ -24,9 +24,14 @@ var platform = function (x, y, type) {
 
   thisPlatform.x = ~~x;
   thisPlatform.y = ~~y;
-
   thisPlatform.type = type;
 
-  return thisPlatform;
+  Platform.prototype.draw = function (ctx) {
+    ctx.fillStyle = thisPlatform.firstColor;
+    ctx.beginPath();
+    ctx.rect(x, y, 200, 20);
+    ctx.closePath();
+    ctx.fill();
+  };
 
 };
